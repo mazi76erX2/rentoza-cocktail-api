92abaf7062a0c8f464f1d164baded5100524fe15
Xolani Mazibuko <mazi76erx@gmail.com>
2023-10-06 16:39:19 +0200

    updated settings file

--- backend/rentoza_cocktail_api/settings.py
+++ backend/rentoza_cocktail_api/settings.py
@@ -85,17 +85,13 @@ WSGI_APPLICATION = "rentoza_cocktail_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME", "cocktail_api"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "root"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
-    # "default": {
-    #     "ENGINE": "django.db.backends.postgresql",
-    #     "NAME": os.getenv("DB_NAME", "cocktail_api"),
-    #     "USER": os.getenv("DB_USER", "postgres"),
-    #     "PASSWORD": os.getenv("DB_PASSWORD", "root"),
-    #     "HOST": os.getenv("DB_HOST", "localhost"),
-    #     "PORT": os.getenv("DB_PORT", "5432"),
-    # }
 }
 
 
